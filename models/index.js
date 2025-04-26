de36f2b9d8e8473c629315dc228778f5bdb67ffb
Aldrindebbarma <aldrin2601@gmail.com>
2025-04-26 12:03:21 +0530

    Delete models directory

--- models/index.js
+++
@@ -1,8 +0,0 @@
-import sequelize from '../config/db.js';
-import MenuItem from './MenuItem.js';
-import Order from './Order.js';
-
-MenuItem.sync();
-Order.sync();
-
-export { sequelize, MenuItem, Order };
