e99694df0eec8beffb9ba86c9dd52de32fc73b6c
Aldrindebbarma <aldrin2601@gmail.com>
2025-04-26 12:03:30 +0530

    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getOrders, placeOrder } from '../controllers/orderController.js';
-
-const router = express.Router();
-router.get('/', getOrders);
-router.post('/', placeOrder);
-
-export default router;
