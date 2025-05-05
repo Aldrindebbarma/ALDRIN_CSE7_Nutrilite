428e0a4daa422527ae67b86958827c875d381221
Aldrindebbarma <aldrin2601@gmail.com>
2025-05-05 13:38:01 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -22,9 +22,8 @@ sequelize
   .sync()
   .then(() => {
     console.log('Database connected');
-    app.listen(5000, () => {
-      console.log('Server running on port 5000');
-    });
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => {
     console.error('Unable to connect to the database:', error);
